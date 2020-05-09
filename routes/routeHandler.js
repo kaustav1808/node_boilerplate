d3dd99f52d6c03d2fad418d304ece77198b56e5a
kaustav <kaustavofficial1808@gmail.com>
2020-05-10 00:04:37 +0530

    starting route resolving

--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -1,5 +1,26 @@
-const routeConfig = require('../config/route')
+const routeConfig   = require('../config/route');
+const routeResolver = require('./routeResolver');
 
-const routehandler = function(req,res,next,routeConfig) {
-    current
+let routeHandler = async function(req,res,callback){
+    const currentPath = req.originalUrl;
+    const basePath    = req.baseUrl;
+
+    if(basePath == 'api'){
+        const resolver = import('./apiRoutes')
+    }else{
+        const resolver = import('./webRoute')
+    }
+
+    resolver.routeResolver(currentPath).then(resolver=>{
+        
+    })
+    
+    try{
+        await resolver.dispatchController(req,res);
+    }catch(error){
+        callback(error)
+    }
+    callback(resolver.getResponse())
 }
+
+exports.default  = routeHandler;
