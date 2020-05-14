6edef195dd7523044aa37c8b35acd38eff58788b
Kaustav Bhattacharya <kaustav.bhattacharya@codelogicx.com>
2020-05-14 22:54:22 +0530

    route resolver.

--- routes/routeHandler.js
+++ routes/routeHandler.js
@@ -1,15 +1,58 @@
 const routeConfig   = require('../config/route');
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
+var express = require('express');
+var router = express.Router();
 
-let routeHandler =  function(req,res,next,callback){
+const pathParams = {
+    currentPath:null,
+    basePath:null,
+    appRootPath:null,
+    routerBasePath:null,
+    pathArray:[],
+}
+
+let routeHandler =  function(req,res,next){
     if(!routeConfig.routePath)
         throw new Error("route path is not set");
 
-    const currentPath = req.originalUrl;res.send(currentPath);
-    const basePath    = req.baseUrl;
-    let   routerBasePath = path.join(__dirname,routeConfig.routePath);
-    let   pathArray     = currentPath.split('/');
+    //initialize path params
+    initPath(req);
+
+    //resolve route modules
+    //resolveRouter();
+
+    console.log(pathParams);
+
     next();
+};
+
+let initPath = function (req){
+      pathParams.currentPath    = req.originalUrl;
+      pathParams.basePath       = req.baseUrl;
+      pathParams.appRootPath    = process.env.PWD;
+      pathParams.routerBasePath = path.join(pathParams.appRootPath,routeConfig.routePath);
+      pathParams.pathArray      = pathParams.currentPath.split('/').shift();
+};
+
+let resolveRouter = async function() {
+    let currentPath = pathParams.routerBasePath + '/';
+
+    if((typeof pathParams.pathArray == 'string') && (pathParams.pathArray)){
+       let pathResolver = 'indexRoute.js';
+
+       try{
+           let module = await import(currentPath+pathResolver);
+       }catch(err){
+           throw new Error(err.message);
+       }
+    }
+
+    pathParams.pathArray.forEach(function(path){
+        if(!path)
+            return;
+
+    })
 };
 
 module.exports  = routeHandler;
