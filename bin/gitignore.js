d8c348b4839a55a82c38b8ac5ee2bd30688922e7
KAUSTAV BHATTACHARYA <kaustavofficial1808@gmail.com>
2020-12-27 15:29:41 +0530

    Create gitignore from a text file

--- bin/gitignore.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-fs.copyFileSync("gitignore.txt",".gitignore",(err)=>{
-    if(error) {
-        console.log("Failed to copy the the file content if gitignore.txt");
-    }else{
-        console.log("File copy successfully done");
-    }
-})
