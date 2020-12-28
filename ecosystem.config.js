a58473f910828577f1240f35a86ef8b93ac71724
KAUSTAV BHATTACHARYA <kaustav.bhattacharya@codelogicx.com>
2020-12-28 12:30:28 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name        : "node-boilerplate-lite-1.0",
+    name        : "node-boilerplate-1.0",
     script      : "./bin/www",
     watch       : true,
   }]
